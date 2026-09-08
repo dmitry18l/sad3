62f5323ded4df197e65520cb8300e5413a8839ce
Dmitry L <dmitrylelin@mail.ru>
2026-09-08 15:07:32 +0700

    First part

---
+++ task1/main.cpp
@@ -0,0 +1,49 @@
+#include "iostream"
+#include "fstream"
+
+using namespace std;
+
+int main() {
+    //Open file for reading
+    ifstream fin("iris34_train.txt");
+
+    //Control: If the file is not next to the program, it will be closed
+    if (!fin.is_open()) {
+        cout << "Error: File iris34_train.txt not found!" << endl;
+        return 1;
+    }
+
+    //Use pointers to allocate memory for a two-dimensional array
+    int rows = 105;
+    int cols = 3; // 3 cloumns: 2 sign and 1 class
+
+    double** a = new double*[rows];
+    for (int i = 0; i < rows; i++) {
+        a[i] = new double[cols];
+    }
+
+    //Read data from the file into our array
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fin >> a[i][j];
+        }
+    }
+    fin.close();
+
+    //Output the already populated array to the screen for verification.
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << a[i][j] << " "; // Стрелочки влево (cout <<) выводят данные на экран
+        }
+        cout << endl;
+    }
+
+    //Clear memory
+    for (int i = 0; i < rows; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
+
+}
